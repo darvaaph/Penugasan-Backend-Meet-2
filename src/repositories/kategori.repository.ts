fcf80d399edc1a13998dba39fa22ad6962a65b96
Darva Hermawan <darva99801@gmail.com>
2026-03-18 04:07:14 +0700

    menambahkan fungsi getKategoriById dan createKategori di kategori.repository.ts

--- src/repositories/kategori.repository.ts
+++ src/repositories/kategori.repository.ts
@@ -1,8 +1,22 @@
 import { DrizzleD1Database } from "drizzle-orm/d1";
 import * as schema from "../db/schema"; 
+import { eq } from "drizzle-orm";
+import { generateId } from "../utils/id";
 
 type DB = DrizzleD1Database<typeof schema>;
 
 export const getAllKategori = async (db: DB) => {
     return await db.select().from(schema.kategori);
+}
+
+export const getKategoriById = async (db: DB, id: string) => {
+    return await db.select().from(schema.kategori).where(eq(schema.kategori.id, id));
+}
+
+export const createKategori = async (db: DB, nama_kategori: string) => {
+    return await db.insert(schema.kategori).values({
+        id: generateId(),
+        nama_kategori: nama_kategori,
+        created_at: new Date().toISOString()
+    });
 }
