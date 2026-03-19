4fa5f52ae0414ad348939da9f387e2a409140013
Darva Hermawan <darva99801@gmail.com>
2026-03-19 14:06:52 +0700

    feat: implement kategori routes for fetching and creating categories with success responses

--- src/routes/kategori.routes.ts
+++ src/routes/kategori.routes.ts
@@ -1,5 +1,9 @@
 import { Hono } from "hono";
+import { drizzle } from "drizzle-orm/d1";
 import { D1Database } from "@cloudflare/workers-types";
+import * as schema from "../db/schema";
+import * as kategoriService from "../services/kategori.service";
+import { successResponse, errorResponse } from "../utils/response";
 
 type Bindings = {
     sistem_manajemen_kantin_db: D1Database;
@@ -8,21 +12,30 @@ type Bindings = {
 const kategoriRoutes = new Hono<{ Bindings: Bindings}>();
 
 kategoriRoutes.get('/', async (c) => {
-    // get all kategori
-})
+    const db = drizzle(c.env.sistem_manajemen_kantin_db, { schema });
+    const data = await kategoriService.getAllKategori(db);
+    return successResponse(c, 200, "Berhasil mengambil daftar kategori", data);
+});
 
 kategoriRoutes.get('/:id', async (c) => {
     // get kategori by id
     const id = c.req.param('id');
+    const db = drizzle(c.env.sistem_manajemen_kantin_db, { schema });
+    const data = await kategoriService.getKategoriById(db, id);
+    return successResponse(c, 200, "Berhasil mengambil kategori", data);
 })
 
 kategoriRoutes.post('/', async (c) => {
     // create kategori
-    const body = await c.req.json();
+    const { nama_kategori } = await c.req.json();
+    const db = drizzle(c.env.sistem_manajemen_kantin_db, { schema });
+    const data = await kategoriService.createKategori(db, nama_kategori);
+    return successResponse(c, 201, "Kategori berhasil ditambahkan", data);
 })
 
 kategoriRoutes.patch('/:id', async (c) => {
     // update kategori
+    
 })
 
 kategoriRoutes.delete('/:id', async (c) => {
