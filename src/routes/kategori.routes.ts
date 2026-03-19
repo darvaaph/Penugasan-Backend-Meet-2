4542a3cf857bbb3e9b8604348fbf577a8f464b98
Darva Hermawan <darva99801@gmail.com>
2026-03-19 13:17:43 +0700

    feat: implement kategori routes for CRUD operations

--- src/routes/kategori.routes.ts
+++ src/routes/kategori.routes.ts
@@ -2,6 +2,28 @@ import { Hono } from "hono";
 
 const kategoriRoutes = new Hono();
 
+kategoriRoutes.get('/', async (c) => {
+    // get all kategori
+})
+
+kategoriRoutes.get('/', async (c) => {
+    // get kategori by id
+    const id = c.req.param('id');
+})
+
+kategoriRoutes.post('/', async (c) => {
+    // create kategori
+    const body = await c.req.json();
+})
+
+kategoriRoutes.patch('/:id', async (c) => {
+    // update kategori
+})
+
+kategoriRoutes.delete('/:id', async (c) => {
+    // delete kategori
+})
+
 
 
 export default kategoriRoutes;
